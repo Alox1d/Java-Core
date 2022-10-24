360bacc5cd29ed4920431a4083a22cd196dd5974
Dmitry Zolotyh <zolotyh.dk@gmail.com>
2022-10-24 14:39:05 +0300

    Add package 'generics' with classes Employee, Game, Participant, Scholar, Student, Team

---
+++ src/generics/Game.java
@@ -0,0 +1,34 @@
+package generics;
+
+public class Game {
+    public static void main(String[] args) {
+        Scholar scholar1 = new Scholar("Игорь", 13);
+        Scholar scholar2 = new Scholar("Мария", 14);
+        Scholar scholar3 = new Scholar("Петр", 12);
+        Scholar scholar4 = new Scholar("Александра", 11);
+
+        Student student1 = new Student("Евгений", 19);
+        Student student2 = new Student("Сергей", 20);
+
+        Employee employee1 = new Employee("Дмитрий", 31);
+        Employee employee2 = new Employee("Алексей", 34);
+
+        Team<Scholar> scholarTeam1 = new Team<>("Везучие");
+        scholarTeam1.addNewParticipant(scholar1);
+        scholarTeam1.addNewParticipant(scholar2);
+
+        Team<Scholar> scholarTeam2 = new Team<>("Красавчики");
+        scholarTeam2.addNewParticipant(scholar3);
+        scholarTeam2.addNewParticipant(scholar4);
+
+        Team<Student> studentTeam = new Team<>("Космос");
+        studentTeam.addNewParticipant(student1);
+        studentTeam.addNewParticipant(student2);
+
+        Team<Employee> employeeTeam = new Team<>("Мечта");
+        employeeTeam.addNewParticipant(employee1);
+        employeeTeam.addNewParticipant(employee2);
+
+        scholarTeam1.playWith(scholarTeam1);
+    }
+}
